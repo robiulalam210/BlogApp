fd8141ac11e4d53bde0168033f89dcdd0718f415
Md Robiul Alam <84795389+robiulalam210@users.noreply.github.com>
2022-12-01 14:19:16 +0600

    Update README.md

--- README.md
+++ README.md
@@ -14,3 +14,9 @@ A few resources to get you started if this is your first Flutter project:
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
+##Output
+
+
+https://user-images.githubusercontent.com/84795389/205001495-1e3b1034-48a8-4e1a-8447-5fe0fac3f5d5.mp4
+
+
--- lib/view/home.dart
+++ lib/view/home.dart
@@ -1,8 +1,10 @@
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:myblog_app/Utlis/utlis.dart';
 import 'package:myblog_app/view/Blog/addpost_blog.dart';
+import 'package:myblog_app/view/Blog/updatepost_blog.dart';
 import 'package:myblog_app/view/signup_login/login.dart';
 import 'package:myblog_app/widget/textformfield.dart';
 
@@ -14,14 +16,30 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  TextEditingController serch_controller = TextEditingController();
-  String serch = "";
   Stream<QuerySnapshot> _stream =
-      FirebaseFirestore.instance.collection("Course").snapshots();
+  FirebaseFirestore.instance.collection("Course").snapshots();
 
+  UpDate(cource_id, cource_title, cource_dis, img) {
+    Navigator.of(context).push(MaterialPageRoute(builder: (builder) =>
+        UpdatePostBlog(docmentID: cource_id,
+            title: cource_title,
+            dis: cource_dis,
+            img: img)));
+  }
   @override
   Widget build(BuildContext context) {
     final auth = FirebaseAuth.instance;
+    Future<void> deleteUser(selectedData) {
+      CollectionReference users =
+      FirebaseFirestore.instance.collection('Course');
+      return users.doc(selectedData).delete().then((value) {
+        print("User Deleted");
+        Utlis().toastMessage("Delet");
+      }).catchError((error) {
+        Utlis().toastMessage(error.toString());
+        print("Failed to delete user: $error");
+      });
+    }
 
     return Scaffold(
         appBar: AppBar(
@@ -35,7 +53,7 @@ class _HomePageState extends State<HomePage> {
                     Navigator.pushAndRemoveUntil(
                         context,
                         MaterialPageRoute(builder: (context) => LoginScreen()),
-                        (route) => false);
+                            (route) => false);
                   }).onError((error, stackTrace) {});
                 },
                 icon: Icon(Icons.logout))
@@ -51,32 +69,6 @@ class _HomePageState extends State<HomePage> {
         body: Container(
           child: Column(
             children: [
-              TextFormField(
-                controller: serch_controller,
-                keyboardType: TextInputType.text,
-                style: TextStyle(color: Colors.black),
-                cursorRadius: Radius.circular(10),
-                cursorColor: Color(0xff8A8A8E),
-                decoration: InputDecoration(
-                    fillColor: Colors.black,
-                    contentPadding: EdgeInsets.all(10),
-                    focusedBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                        borderSide: BorderSide(color: Colors.blue)),
-                    enabledBorder: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                        borderSide: BorderSide(color: Color(0xff8A8A8E))),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(20))),
-                    prefixIcon: Icon(Icons.search),
-                    prefixIconColor: Color(0xff8A8A8E).withOpacity(0.7),
-                    suffixIconColor: Color(0xff8A8A8E).withOpacity(0.7),
-                    hintText: "Serch...",
-                    hintStyle: TextStyle(color: Color(0xff8A8A8E))),
-                onChanged: (String value) {
-                  serch = value;
-                },
-              ),
               Expanded(
                 child: StreamBuilder<QuerySnapshot>(
                   stream: _stream,
@@ -84,10 +76,7 @@ class _HomePageState extends State<HomePage> {
                       AsyncSnapshot<QuerySnapshot> snapshot) {
                     if (snapshot.hasError) {
                       print("eroooooooo");
-                    }
-
-
-                    else if (snapshot.connectionState ==
+                    } else if (snapshot.connectionState ==
                         ConnectionState.waiting) {
                       return Center(
                         child: CircularProgressIndicator(),
@@ -95,180 +84,88 @@ class _HomePageState extends State<HomePage> {
                     }
                     return ListView(
                       children:
-                          snapshot.data!.docs.map((DocumentSnapshot document) {
+                      snapshot.data!.docs.map((DocumentSnapshot document) {
                         Map<String, dynamic> data =
-                            document.data() as Map<String, dynamic>;
+                        document.data() as Map<String, dynamic>;
 
-                        String tempTitle=data["title"];
-                        if(serch_controller.text.isEmpty){
-                          return Container(
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10)),
-                            padding: EdgeInsets.all(5),
-                            height: MediaQuery.of(context).size.height * 0.2,
-                            child: Card(
-                              elevation: 5,
-                              child: Row(
-                                children: [
-                                  Expanded(
-                                      flex: 1,
-                                      child: Container(
-                                        padding: EdgeInsets.all(10),
-                                        child: ClipRRect(
-                                            borderRadius:
-                                            BorderRadius.circular(12),
-                                            child: Image.network(data["img"])),
-                                      )),
-                                  Expanded(
-                                      flex: 1,
-                                      child: Container(
-                                        padding: EdgeInsets.all(10),
-                                        child: Column(
-                                          mainAxisAlignment:
-                                          MainAxisAlignment.start,
-                                          crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                          children: [
-                                            SizedBox(
-                                              height: MediaQuery.of(context)
-                                                  .size
-                                                  .height *
-                                                  0.01,
-                                            ),
-                                            Text(
-                                              data["title"],
-                                              style: TextStyle(fontSize: 22),
-                                            ),
-                                            SizedBox(
-                                              height: MediaQuery.of(context)
-                                                  .size
-                                                  .height *
-                                                  0.01,
-                                            ),
-                                            Text(data["dis"],
-                                                maxLines: 4,
-                                                style: TextStyle(fontSize: 16)),
-                                          ],
-                                        ),
-                                      ))
-                                ],
-                              ),
+                        return Container(
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10)),
+                          padding: EdgeInsets.all(5),
+                          height: MediaQuery
+                              .of(context)
+                              .size
+                              .height * 0.3,
+                          child: Card(
+                            elevation: 5,
+                            child: Row(
+                              children: [
+                                Expanded(
+                                    flex: 1,
+                                    child: Container(
+                                      padding: EdgeInsets.all(10),
+                                      child: ClipRRect(
+                                          borderRadius:
+                                          BorderRadius.circular(12),
+                                          child: Image.network(data["img"])),
+                                    )),
+                                Expanded(
+                                    flex: 1,
+                                    child: Container(
+                                      padding: EdgeInsets.all(10),
+                                      child: Column(
+                                        mainAxisAlignment:
+                                        MainAxisAlignment.start,
+                                        crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                        children: [
+                                          SizedBox(
+                                            height: MediaQuery
+                                                .of(context)
+                                                .size
+                                                .height *
+                                                0.01,
+                                          ),
+                                          Text(
+                                            data["title"],
+                                            style: TextStyle(fontSize: 22),
+                                          ),
+                                          SizedBox(
+                                            height: MediaQuery
+                                                .of(context)
+                                                .size
+                                                .height *
+                                                0.01,
+                                          ),
+                                          Text(data["dis"],
+                                              maxLines: 4,
+                                              style: TextStyle(fontSize: 16)),
+                                          Row(
+                                            children: [
+                                              IconButton(
+                                                  onPressed: () {
+                                                    UpDate(document.id,
+                                                        data["title"],
+                                                        data["dis"],
+                                                        data["img"]);
+                                                  },
+                                                  icon: Icon(Icons.edit)),
+                                              IconButton(
+                                                  onPressed: () {
+                                                    setState(() {
+                                                      deleteUser(document.id);
+                                                    });
+                                                  },
+                                                  icon: Icon(Icons.delete)),
+                                            ],
+                                          )
+                                        ],
+                                      ),
+                                    ))
+                              ],
                             ),
-                          );
-                        }
-                        else if(tempTitle.toLowerCase().contains(serch_controller.text.toString())){
-                          return Container(
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10)),
-                            padding: EdgeInsets.all(5),
-                            height: MediaQuery.of(context).size.height * 0.2,
-                            child: Card(
-                              elevation: 5,
-                              child: Row(
-                                children: [
-                                  Expanded(
-                                      flex: 1,
-                                      child: Container(
-                                        padding: EdgeInsets.all(10),
-                                        child: ClipRRect(
-                                            borderRadius:
-                                            BorderRadius.circular(12),
-                                            child: Image.network(data["img"])),
-                                      )),
-                                  Expanded(
-                                      flex: 1,
-                                      child: Container(
-                                        padding: EdgeInsets.all(10),
-                                        child: Column(
-                                          mainAxisAlignment:
-                                          MainAxisAlignment.start,
-                                          crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                          children: [
-                                            SizedBox(
-                                              height: MediaQuery.of(context)
-                                                  .size
-                                                  .height *
-                                                  0.01,
-                                            ),
-                                            Text(
-                                              data["title"],
-                                              style: TextStyle(fontSize: 22),
-                                            ),
-                                            SizedBox(
-                                              height: MediaQuery.of(context)
-                                                  .size
-                                                  .height *
-                                                  0.01,
-                                            ),
-                                            Text(data["dis"],
-                                                maxLines: 4,
-                                                style: TextStyle(fontSize: 16)),
-                                          ],
-                                        ),
-                                      ))
-                                ],
-                              ),
-                            ),
-                          );
-                        }
-                        else{
-                          return Container(
-                            decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(10)),
-                            padding: EdgeInsets.all(5),
-                            height: MediaQuery.of(context).size.height * 0.2,
-                            child: Card(
-                              elevation: 5,
-                              child: Row(
-                                children: [
-                                  Expanded(
-                                      flex: 1,
-                                      child: Container(
-                                        padding: EdgeInsets.all(10),
-                                        child: ClipRRect(
-                                            borderRadius:
-                                            BorderRadius.circular(12),
-                                            child: Image.network(data["img"])),
-                                      )),
-                                  Expanded(
-                                      flex: 1,
-                                      child: Container(
-                                        padding: EdgeInsets.all(10),
-                                        child: Column(
-                                          mainAxisAlignment:
-                                          MainAxisAlignment.start,
-                                          crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                          children: [
-                                            SizedBox(
-                                              height: MediaQuery.of(context)
-                                                  .size
-                                                  .height *
-                                                  0.01,
-                                            ),
-                                            Text(
-                                              data["title"],
-                                              style: TextStyle(fontSize: 22),
-                                            ),
-                                            SizedBox(
-                                              height: MediaQuery.of(context)
-                                                  .size
-                                                  .height *
-                                                  0.01,
-                                            ),
-                                            Text(data["dis"],
-                                                maxLines: 4,
-                                                style: TextStyle(fontSize: 16)),
-                                          ],
-                                        ),
-                                      ))
-                                ],
-                              ),
-                            ),
-                          );
-                        }
-
+                          ),
+                        );
                       }).toList(),
                     );
                   },
@@ -279,3 +176,5 @@ class _HomePageState extends State<HomePage> {
         ));
   }
 }
+
+
